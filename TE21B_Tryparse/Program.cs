6e5359bd53ad041b89481aa41b8641b7f96625c0
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-11-09 14:54:11 +0100

    Initial commit

---
+++ TE21B_Tryparse/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+List<string> names = new List<string>();
+
+names.Add("Micke");
+
+
+
+Console.ReadLine();
+
+// int num = 0;
+// bool success = false;
+
+// while (!success)
+// {
+//   Console.WriteLine("Skriv in en siffra:");
+//   string numText = Console.ReadLine();
+
+//   success = int.TryParse(numText, out num);
+
+//   if (!success)
+//   {
+//     Console.WriteLine(" En siffra sa jag, dumhuvud!");
+//   }
+// }
+
+// Console.WriteLine(num);
+
